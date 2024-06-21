dcbba807b3a140dde3f16ade278d2439feea5988
Diogo Rodrigo Gonçalves <goncalvesdiogo@gmail.com>
2024-06-21 11:47:52 -0300

    backup

--- docker/scripts/01_init_secretsmanager.sh
+++ docker/scripts/01_init_secretsmanager.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 aws --endpoint http://localhost:4566 secretsmanager create-secret --name secret/mysql --secret-string "{\"username\":\"root\",\"password\":\"password\"}" --region us-east-1
 aws --endpoint http://localhost:4566 secretsmanager create-secret --name secret/application --secret-string "{\"username\":\"root\",\"password\":\"password\"}" --region us-east-1

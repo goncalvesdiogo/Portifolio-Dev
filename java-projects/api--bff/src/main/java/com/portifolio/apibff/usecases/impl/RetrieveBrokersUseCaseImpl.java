d4e4ad2c09e9881269ab3571ba525108068b436e
goncalvesdiogo <goncalvesdiogo@gmail.com>
2024-09-04 13:46:46 -0300

    Update to RestClient removing OpenFeign

--- java-projects/api--bff/src/main/java/com/portifolio/apibff/usecases/impl/RetrieveBrokersUseCaseImpl.java
+++ java-projects/api--bff/src/main/java/com/portifolio/apibff/usecases/impl/RetrieveBrokersUseCaseImpl.java
@@ -1,12 +1,17 @@
 package com.portifolio.apibff.usecases.impl;
 
+
 import com.portifolio.apibff.entities.domain.BrokerEntity;
-import com.portifolio.apibff.repositories.http.ServiceStocksBrokersFeignClient;
+import com.portifolio.apibff.repositories.dtos.BrokerResponse;
 import com.portifolio.apibff.repositories.mappers.BrokerRepositoryToEntityMapper;
 import com.portifolio.apibff.usecases.RetrieveBrokersUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,23 +21,26 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RetrieveBrokersUseCaseImpl implements RetrieveBrokersUseCase {
 
-    private final ServiceStocksBrokersFeignClient feignClient;
     private final BrokerRepositoryToEntityMapper mapper;
-
+    private final DiscoveryClient discoveryClient;
+    private ServiceInstance serviceInstance = discoveryClient.getInstances("servicestock").get(0);
 
     @Override
     public List<BrokerEntity> getAll() {
-        var result = feignClient.getAll();
-        return result.getBody().stream().map(mapper::from).collect(Collectors.toList());
-
-
+        var result = RestClient.builder().build().get()
+                .uri(serviceInstance.getUri() + "/service-stock/brokers")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<BrokerResponse>>() {});
+        return result.stream().map(mapper::from).collect(Collectors.toList());
     }
 
     @Override
     public BrokerEntity findById(Integer id) {
-        var result = feignClient.getById(id);
-        return mapper.from(result.getBody());
+        var result = RestClient.builder().build().get()
+                .uri(serviceInstance.getUri() + "/service-stock/brokers/{id}", id)
+                .retrieve()
+                .body(new ParameterizedTypeReference<BrokerResponse>() {});
+        return mapper.from(result);
     }
-
 
 }
